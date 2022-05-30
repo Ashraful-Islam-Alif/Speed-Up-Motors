a73eb30cc6e8109462828f4efbb72fd4cd361280
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-05-30 20:06:52 +0600

    payment method completed

--- src/Pages/Home/DashBoard/Payment.js
+++ src/Pages/Home/DashBoard/Payment.js
@@ -1,8 +1,12 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Loading';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51L51bZCZ4BDWrDsU72y5MjwEabOirFnzo6Ee30kzkZx5UVixxmE6vVl7hrrrGopOoch08AhUUYVdSCO8czBnYGm500RSVNzqUh');
 const Payment = () => {
 
     const { id } = useParams();
@@ -28,7 +32,13 @@ const Payment = () => {
                     <p>Please pay: ${order?.price}</p>
                 </div>
             </div>
-
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm order={order} />
+                    </Elements>
+                </div>
+            </div>
         </div>
     );
 };
